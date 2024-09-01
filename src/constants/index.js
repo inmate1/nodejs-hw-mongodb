6a58511d0e75fa3e6fbd5cd27bf7cf05e3e9e77c
Aleksander <aleksander.0705@gmail.com>
2024-09-01 19:22:14 +0300

    add swagger

--- src/constants/index.js
+++ src/constants/index.js
@@ -27,3 +27,5 @@ export const CLOUDINARY = {
   API_SECRET: 'API_SECRET',
   ENABLE_CLOUDINARY: 'ENABLE_CLOUDINARY',
 };
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import swaggerDocs from './middlewares/swaggerDocs.js';
 
 const setupServer = () => {
   const app = express();
@@ -15,6 +16,7 @@ const setupServer = () => {
   app.use(cookieParser());
   app.use(logger);
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(router);
 
   app.use('*', notFoundHandler);
